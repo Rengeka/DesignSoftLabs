d64adb2fec6cff49bf38ac1458de29d01b01b6a9
Rengeka <stasik2004x@gmail.com>
2025-09-11 21:03:13 +0300

    Added Lab1

---
+++ Lab1/Lab1.Application/Services/UserService.cs
@@ -0,0 +1,15 @@
+﻿using Lab1.Application.Contracts.Repositories;
+using Lab1.Application.Contracts.Services;
+using Lab1.Application.Enums;
+using Lab1.Domain;
+
+namespace Lab1.Application.Services;
+
+public class UserService(IUserRepository userRepository) : IUserService
+{
+    public async Task<User?> GetUserAsync(string name, GetUserFieldMask fieldMask, CancellationToken cancellationToken) 
+    {
+        var u = await userRepository.FindByNameAsync(name, fieldMask, cancellationToken);
+        return await userRepository.FindByNameAsync(name, fieldMask, cancellationToken);
+    }
+}
